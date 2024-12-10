46fc9a4aaa616e038d22f566c2f90caf790b07a7
tanvirahmedrume <tstanvir371806@gmail.com>
2024-12-10 23:26:07 +0600

    added serach bar - with js

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,13 @@ const navClose = document.querySelector('.navClose');
 const navShow = document.querySelector('.navShow');
 const hideLogo = document.querySelector('.hideLogo');
 const hiddenSearch = document.querySelector('.hiddenSearch');
+// JavaScript for toggling search popup
+const searchPopup = document.querySelector('.searchPopup');
+const pageContent = document.querySelector('.pageContent');
+const openSearch = document.querySelector('.openSearch');
+const closeSearch = document.querySelector('.closeSearch');
+const womanImage = document.querySelector('.womanImage');
+const body = document.body;
 
 
 navOpen.addEventListener('click', ()=>{
@@ -18,4 +25,20 @@ navClose.addEventListener('click', ()=>{
     navOpen.classList.remove('hidden');
     hideLogo.classList.remove('hideLogo');
     hiddenSearch.classList.remove('hiddenSearch');
+});
+
+// Open search
+openSearch.addEventListener('click', () =>{
+  searchPopup.classList.remove('hidden');
+  pageContent.classList.add('blur-sm');
+  womanImage.classList.remove('relative');
+  body.classList.add('overflow-hidden'); // Disable scrolling
+});
+
+// Close search
+closeSearch.addEventListener('click', () => {
+  searchPopup.classList.add('hidden');
+  pageContent.classList.remove('blur-sm');
+  womanImage.classList.add('relative');
+  body.classList.remove('overflow-hidden'); // Enable scrolling
 });
